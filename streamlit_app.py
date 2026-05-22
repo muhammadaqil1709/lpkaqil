4c44eba4f5d035c875d5bb4ed1a3e8f2ce74ecbf
muhammadaqil1709 <mhd.aqill17@gmail.com>
2026-05-22 14:25:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,2 @@
-import streamlit as st
-
-st.title("🎈 Lpk Aqil")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("This is a header with a divider", divider="gray")
-st.header("These headers have rotating dividers", divider=True)
-st.header("One", divider=True)
-st.header("Two", divider=True)
-st.header("Three", divider=True)
-st.header("Four", divider=True)
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
-st.caption("This is a string that explains something above.")
-st.caption("A caption with _italics_ :blue[colors] and emojis :sunglasses:")
